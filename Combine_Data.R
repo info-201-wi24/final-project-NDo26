22625fea9052f7d14c9c41837e5968e3b4f59996
jolieng12 <jolieng@uw.edu>
2024-02-17 21:46:05 -0800

    add the Is_NBA column

--- Combine_Data.R
+++ Combine_Data.R
@@ -4,3 +4,11 @@ library(dplyr)
 
 wnba_2022 <- read.csv("2022_WNBA_stats.csv")
 nba_2022_2023 <- read.csv("NBA_Player_Salaries.csv")
+
+wnba_2022 <- 
+  wnba_2022 %>%
+  mutate(Is_NBA = 0)
+
+nba_2022_2023 <- 
+  nba_2022_2023 %>%
+  mutate(IS_NBA = 1)
