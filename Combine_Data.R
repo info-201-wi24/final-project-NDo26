f183e068ab1225b2cdcf187f71b959edd0e2811d
jolieng12 <jolieng@uw.edu>
2024-02-17 21:41:23 -0800

    csv to df

--- Combine_Data.R
+++ Combine_Data.R
@@ -1,1 +1,7 @@
 # Nathan, Joey, Makayla, Nowali
+
+library(dplyr)
+
+wnba_2023 <- read.csv("2023_WNBA_stats.csv")
+wnba_2022 <- read.csv("2022_WNBA_stats.csv")
+nba_2022_2023 <- read.csv("NBA_Player_Salaries.csv")
