eb0d93f89a34b3567d359a18ad66244906ee2f6c
NDo26 <126648250+NDo26@users.noreply.github.com>
2024-03-04 18:11:43 -0800

    Add files via upload

--- Combine_Data.R
+++ Combine_Data.R
@@ -23,7 +23,44 @@ wnba_nba <- full_join(nba_2022_2023, wnba_2022, by = c("Player.Name" = "Player",
                                                        "MP" = "MIN", #minutes per game
                                                        "FG" = "FGM", #field goals
                                                        "FGA" = "FGA", #field goals attempted
-                                                       "Is_NBA"))  
+                                                       "Is_NBA"))
+
+wnba_nba <- wnba_nba %>%
+  mutate(X3PA = coalesce(X3PA.x, X3PA.y),
+         X3P = coalesce(X3P, X3PM),
+         X2P = coalesce(X2P, X2PM),
+         X2PA = coalesce(X2PA.x, X2PA.y),
+         FT = coalesce(FT, FTM),
+         FTA = coalesce(FTA.x, FTA.y),
+         ORB = coalesce(ORB.x, ORB.y),
+         DRB = coalesce(DRB.x, DRB.y),
+         TRB = coalesce(TRB.x, TRB.y),
+         AST = coalesce(AST.x, AST.y),
+         STL = coalesce(STL.x, STL.y),
+         BLK = coalesce(BLK.x, BLK.y),
+         TOV = coalesce(TOV.x, TOV.y),
+         PF = coalesce(PF.x, PF.y)
+  ) %>%
+  select(-X3PA.x, -X3PA.y, -X3PM, -X2PM, -X2PA.x, -X2PA.y, -FTM, -ORB.x, -ORB.y, -DRB.x, -DRB.y, -TRB.x, 
+         -TRB.y, -AST.x, -AST.y, -STL.x, -STL.y, -BLK.x, -BLK.y, -TOV.x, -TOV.y, -PF.x, -PF.y) %>%
+  rename("Games Played" = "GP",
+         "Games Started" = "GS",
+         "Minutes Played" = "MP",
+         "Field Goals Made" = "FG",
+         "Field Goals Attempted" = "FGA",
+         "3-Pointers Made" = "X3P",
+         "3-Pointers Attempted" = "X3PA",
+         "Free-Throws Made" = "FT",
+         "Free-Throws Attempted" = "FTA",
+         "Offensive Rebounds" = "ORB",
+         "Defensive Rebounds" = "DRB",
+         "Total Rebounds" = "TRB",
+         "Assists" = "AST",
+         "Steals" = "STL",
+         "Blocks" = "BLK",
+         "Turnovers" = "TOV",
+         "Personal Fouls" = "PF"
+  )
 
 # average salary overall
 average_salary_wnba <- mean(wnba_2022$X2022.Salary, na.rm = TRUE)
@@ -39,3 +76,5 @@ avg_salary <- tibble(
 )
 
 write.csv(wnba_nba, "wnba_nba_data.csv")
+
+
